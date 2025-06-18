5f7e1f2d2ed9ffd7e3f5416e6288cb9440c387e4
Lucas Lima <limalucas7027@gmail.com>
2025-06-17 22:12:11 -0300

    Movie model fix
    Fixed the model Movies to point to posters folder
    created a view to get all movies

--- movies/models.py
+++ movies/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+import os
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class Movie(models.Model):
     name = models.CharField(max_length=500)
     synopsis = models.CharField(max_length=5000)
     duration = models.IntegerField()
-    poster = models.FilePathField()
+    poster = models.FilePathField(path=os.path.join(APP_DIR, 'posters'), match=".*\.(jpg|jpeg|png)$", recursive=True, allow_files=True,allow_folders=False)
     director = models.ManyToManyField("Director")
 
     def __str__(self):
         return self.name
     
-
 
 class Director(models.Model):
     name = models.CharField(max_length=500)
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Movie, Director
+from django.http import JsonResponse
+from django.core import serializers
 
+def get_all_movies(request):
+    all_movies = serializers.serialize("json", format=str ,Movie.objects.all()) 
+    return JsonResponse(all_movies, safe=False)
 # Create your views here.
