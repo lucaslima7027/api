e8074d0c693bf1bfcd3897d2953b87e3de97788e
Lucas Lima <limalucas7027@gmail.com>
2025-06-17 21:18:46 -0300

    Created two models The Movie model contains the basic infos about a movie The Director model contains the basic infos about a director

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class Movie(models.Model):
+    name = models.CharField(max_length=500)
+    synopsis = models.CharField(max_length=5000)
+    duration = models.IntegerField()
+    poster = models.FilePathField()
+    director = models.ManyToManyField("Director")
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=500)
+    age = models.IntegerField()
+    nationality = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return self.name
 # Create your models here.
